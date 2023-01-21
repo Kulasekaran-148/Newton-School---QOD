e613694af14c62ce4516aecaf0d6a6b9704196ce
Kulasekaran <kulasekaranslrk@gmail.com>
2023-01-22 00:06:42 +0530

    Added clean code for abhas number problem

--- (21.01.2023) - Abhas and Numbers/Abhas_and_numbers.c
+++ (21.01.2023) - Abhas and Numbers/Abhas_and_numbers.c
@@ -51,3 +51,54 @@ int main() {
     }
     return 0;
 }
+
+Alternate code:
+
+#include <stdio.h>
+#include <math.h>
+long power(long value, long exponent)
+{
+    int i;
+    long result=1;
+    for(i=1;i<=exponent;i++)
+    {result*=value;}
+    return result;
+}
+int main() {
+    long n,k;
+    scanf("%ld %ld",&n,&k);
+    long num_int=(int)n/k;
+    double num_float = (double)n/k;
+    if(n<k)
+    {
+        if(n%2==0 || k%2==0)
+        {
+        if(num_float>=0.5)
+            { printf("%d",1); }
+        else
+            { printf("%d",0); }
+        }
+        else
+        {printf("%d",0);}
+    }
+    else if(n%k==0) //Divisible
+    {
+        if(n%2==0 && k%2==0) //Both n & k are even. ans is ((num_int)^3)*2
+        { printf("%ld",(power((num_int),3))*2); }
+        else //n is even, k is odd (or) both n and k are odd, ans is (num_int)^3
+            { printf("%ld",power((num_int),3)); }
+    }
+    else //Non-Divisible
+    {
+        if((n%2!=0 && k%2==0) || (n%2==0 && k%2==0))//n is odd & k is even (or) Both n & k are even 
+        {
+            if((num_float-(int)num_float)<0.5) //if decimal part of n/k is less than 50%, ans is ((num_int)^3)*2
+                { printf("%ld",(power((num_int),3))*2); }
+            else //if decimal part of n/k is more than 50%, ans is (num_int)^3 + (num_int +1)^3
+                { printf("%ld", power((num_int),3)+power((num_int+1),3)); }
+        }
+        else //n is even, k is odd (or) both n and k are odd, ans is (num_int)^3
+            { printf("%ld",power((num_int),3)); }
+    }
+    return 0;
+}
